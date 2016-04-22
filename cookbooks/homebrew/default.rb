0eef049e5623c9f3961c82a5dd171a3f5ff2c098
reizist <reizist@gmail.com>
2016-04-23 02:51:23 +0900

    Modify homebrew recipe for installing formulas

--- cookbooks/homebrew/default.rb
+++ cookbooks/homebrew/default.rb
@@ -3,3 +3,11 @@ execute "install homebrew" do
   www_script_path = 'https://raw.githubusercontent.com/Homebrew/install/master/install'
   command "ruby -e $(curl -fsSL #{www_script_path})"
 end
+
+node.reverse_merge!(
+  brew_packages: [],
+)
+
+node[:brew_packages].each do |name|
+  package name
+end
