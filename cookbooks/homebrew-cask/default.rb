dc7100a8de32b9bbf9bc9fcfc340fdaac57922be
reizist <reizist@gmail.com>
2016-04-23 02:33:42 +0900

    Modify homebrew-cask recipe for installing Cask

--- cookbooks/homebrew-cask/default.rb
+++ cookbooks/homebrew-cask/default.rb
@@ -1,6 +1,19 @@
+require "itamae/plugin/resource/cask"
 include_recipe "../homebrew/default.rb"
 
 execute "install brew-cask" do
   not_if "brew cask"
   command "brew tap caskroom/cask"
+end
+
+execute "export app_dir for brew-cask" do
+  command 'export HOMEBREW_CASK_OPTS="--appdir=/Applications"'
+end
+
+node.reverse_merge!(
+  cask_packages: [],
+)
+
+node[:cask_packages].each do |name|
+  cask name
 end
